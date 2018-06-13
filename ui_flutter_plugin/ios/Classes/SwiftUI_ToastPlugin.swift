bfaf5dd47fbef7ac90eb183f8d8898c8b200d0b9
yangyxd <yangyxd@126.com>
2018-06-13 17:50:44 +0800

    .

--- ui_flutter_plugin/ios/Classes/SwiftUI_ToastPlugin.swift
+++
@@ -1,6 +0,0 @@
-import Flutter
-import UIKit
-    
-public class SwiftUI_ToastPlugin: NSObject {
-
-}
--- ui_flutter_plugin/ios/Classes/UiFlutterPlugin.h
+++
@@ -1,4 +0,0 @@
-#import <Flutter/Flutter.h>
-
-@interface UiFlutterPlugin : NSObject<FlutterPlugin>
-@end
